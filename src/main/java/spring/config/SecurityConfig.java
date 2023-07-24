184b662cea9ed1620765bf6a5b53fd34de9f305c
gubersky <https://github.com/gubersky/Burger.git>
2023-07-24 18:37:19 +0300

    refactor

--- src/main/java/spring/config/SecurityConfig.java
+++ src/main/java/spring/config/SecurityConfig.java
@@ -2,53 +2,50 @@ package spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
 import javax.sql.DataSource;
-
-import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.formLogin(withDefaults())
                 .authorizeHttpRequests((auth) ->
-                        auth.anyRequest().authenticated());
+                        auth.requestMatchers(HttpMethod.POST, "/order")
+                                .hasRole("USER")
+                                .anyRequest()
+                                .authenticated());
         return http.build();
     }
 
     @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(H2)
-                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-                .build();
-    }
-
-    @Bean
-    public UserDetailsService users(PasswordEncoder passwordEncoder, DataSource dataSource) {
+    public UserDetailsService users(DataSource dataSource, PasswordEncoder passwordEncoder) {
         UserDetails user = User.builder()
-                .username("User")
-                .password(passwordEncoder.encode("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW"))
+                .username("marouned")
+                .password(passwordEncoder.encode("marouned"))
                 .roles("USER")
                 .build();
         UserDetails admin = User.builder()
-                .username("Admin")
-                .password(passwordEncoder.encode("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW"))
+                .username("admin")
+                .password(passwordEncoder.encode("password"))
                 .roles("USER", "ADMIN")
                 .build();
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
